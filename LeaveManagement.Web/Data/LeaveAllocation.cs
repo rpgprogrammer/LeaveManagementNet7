fac01bcdd0cae953b0ee193a5278191516e45f73
Paul Radu <paulg@PAUL>
2023-03-04 12:50:31 +0200

    Added Tables Leave ype and LeaveAllocations

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation :BaseEntity
+    {
+       
+
+        public DateTime NumberOfDays { get; set; }
+
+        public LeaveType LeaveType { get; set; }
+        [ForeignKey("LeaveTypeId")] // optional 
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+       
+       
+
+
+    }
+}
